03bb9b20c5c4a0ac7d5f4897ca58ac4157c1ebc1
younes <younes@interactive-object.com>
2013-10-07 01:33:05 +0200

    fix p

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-/**
- * Module dependencies.
- */
-
 var express = require('express'), 
 	http = require('http'),
-	events = require('events'),
 	util = require('util'),
 	app = express(),
 	_ = require('underscore'),
@@ -12,32 +7,36 @@ var express = require('express'),
 
 
 
-var SerialPort = require("serialport").SerialPort;
-var serial = new SerialPort("/dev/tty.FireFly-E6F7-SPP", { baudrate : 57600 });
-serial.on("data", function (sdata) {
-  
-});
 
-serial.on("open", function (sdata) {
-  console.log('Serial opened');
-});
+	var SerialPort = require("serialport").SerialPort;
+	var serial = new SerialPort("/dev/tty.FireFly-E6F7-SPP", { baudrate : 57600 });
+	
 
+	serial.on("open", function (sdata) {
+	  console.log('Serial opened');
+		serial.on("data", function (sdata) {
   
+		});
+	});
+	
+
 app.configure(function () {
 	app.set('port', process.env.PORT || 3003);
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
 	app.use(express.favicon());
-	app.use(express.logger('dev'));
+	//app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	//app.use(express.static(__dirname + '/public'));
 	app.use(express.static(require('path').resolve(__dirname + "/public")));
 });
 
+
 app.configure('development', function () {
 	app.use(express.errorHandler());
 });
+
 
 app.get('/', function (req, res) {
 	res.render('index');
@@ -54,7 +53,6 @@ app.get('/', function (req, res) {
 //     			: 210 -meduim speed
 //     			: 220 -slow speed    
 // ----------------------------------------------
-// ----------------------------------------------
 // 
 // - brake		: 300
 // - left			: 40
@@ -67,25 +65,25 @@ var server = app.listen(app.get('port'));
 var everyone = require("now").initialize(server);
 
 everyone.now.forward = function(callback){
-  serial.write(100);
+  //serial.write(100);
 }
 
 everyone.now.backward = function(callback){
-  serial.write(200);
+  //serial.write(200);
 }
 
 everyone.now.left = function(callback){
-  serial.write(40);
+  //serial.write(40);
 }
 
 everyone.now.right = function(callback){
-  serial.write(50);
+  //serial.write(50);
 }
 
 everyone.now.brake = function(callback){
-  serial.write(300);
+  //serial.write(300);
 }
 
 everyone.now.straight = function(callback){
-  serial.write(10);
+  //serial.write(10);
 }
