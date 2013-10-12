586e41f1301cbc8b5827dfa8a69d0d834652ce3e
Younes <younes@interactive-object.com>
2013-10-13 00:16:44 +0200

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,43 +1,72 @@
 $(function(){
 
 now.ready(function(){
-    // "Hello World!" will print on server
-    console.log("now ready");
-  
+    
+     var socket = io.connect(),
+    wIsDown = false,
+    aIsDown = false,
+    sIsDown = false,
+    dIsDown = false;
+
+  $(document).keydown(function(e){
+    switch(e.which){
+      case 87:
+        if(wIsDown) return;
+        wIsDown = true;
+        now.forward();
+        $('.up').addClass('active');
+        break;
+      case 65:
+        if(aIsDown) return;
+        aIsDown = true;
+        now.left();
+        $('.left').addClass('active');
+        break;
+      case 83:
+        if(sIsDown) return;
+        sIsDown = true;
+        now.backward();
+        $('.down').addClass('active');
+        break;
+      case 68:
+        if(dIsDown) return;
+        dIsDown = true;
+        now.left();
+        $('.right').addClass('active');
+        break;
+    }
+  });
+
+  $(document).keyup(function(e){
+    switch(e.which){
+      case 87:
+        if(!wIsDown) return;
+        wIsDown = false;
+        now.stop();
+        $('.up').removeClass('active');
+        break;
+      case 65:
+        if(!aIsDown) return;
+        aIsDown = false;
+        now.staight();
+        $('.left').removeClass('active');
+        break;
+      case 83:
+        if(!sIsDown) return;
+        sIsDown = false;
+        now.stop();
+        $('.down').removeClass('active');
+        break;
+      case 68:
+        if(!dIsDown) return;
+        dIsDown = false;
+        now.staight();
+        $('.right').removeClass('active');
+        break;
+    }
+  });
+    
 });
-	$(".up").mouseup(function(){
-		$.post( "/stop");
-	});
-	
-  $(".up").mousedown(function(){
-    $.post( "/forward");
-  });
-	
-	
-  $(".down").mousedown(function(){
-    $.post( "/backward");
-  });
-
-  $(".down").mouseup(function(){
-    $.post( "/stop");
-  });
-	
-	
-  $(".left").mousedown(function(){
-    $.post( "/left");
-  });       
-
-  $(".right").mousedown(function(){
-    $.post( "/right");
-  });       
-
-
-  $(".left").mouseup(function(){
-    $.post( "/straight");
-  });       
-
-  $(".right").mouseup(function(){
-    $.post( "/straight");
-  });  
+  
 
 });
