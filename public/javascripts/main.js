8af6d6da436e5b3231cdd29a541daae3119aebb9
Younes <younes@interactive-object.com>
2013-10-13 00:25:24 +0200

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -14,25 +14,25 @@ now.ready(function(){
       console.log(now);
       console.log(window.now);
     switch(e.which){
-      case 87:
+      case 38:
         if(wIsDown) return;
         wIsDown = true;
         now.forward();
         $('.up').addClass('active');
         break;
-      case 65:
+      case 37:
         if(aIsDown) return;
         aIsDown = true;
         now.left();
         $('.left').addClass('active');
         break;
-      case 83:
+      case 40:
         if(sIsDown) return;
         sIsDown = true;
         now.backward();
         $('.down').addClass('active');
         break;
-      case 68:
+      case 39:
         if(dIsDown) return;
         dIsDown = true;
         now.left();
@@ -43,25 +43,25 @@ now.ready(function(){
 
   $(document).keyup(function(e){
     switch(e.which){
-      case 87:
+      case 38:
         if(!wIsDown) return;
         wIsDown = false;
         now.stop();
         $('.up').removeClass('active');
         break;
-      case 65:
+      case 37:
         if(!aIsDown) return;
         aIsDown = false;
         now.staight();
         $('.left').removeClass('active');
         break;
-      case 83:
+      case 40:
         if(!sIsDown) return;
         sIsDown = false;
         now.stop();
         $('.down').removeClass('active');
         break;
-      case 68:
+      case 39:
         if(!dIsDown) return;
         dIsDown = false;
         now.staight();
